99e91175e790095e16c636b5435083f70aec70e3
Winston Chang <winston@stdout.org>
2013-04-22 15:46:31 -0500

    Update datepicker

--- datepicker/server.r
+++ datepicker/server.r
@@ -1,8 +1,10 @@
 shinyServer(function(input, output, clientData, session) {
 
-  output$date  <- renderText({ input$date  })
-  output$date2 <- renderText({ input$date2 })
+  # input$date and others are Date objects; need to convert to character,
+  # otherwise it will print an integer rather than a date.
+  output$date  <- renderText({ as.character(input$date)  })
+  output$date2 <- renderText({ as.character(input$date2) })
 
-  output$daterange  <- renderText({ input$daterange  })
-  output$daterange2 <- renderText({ input$daterange2 })
+  output$daterange  <- renderText({ as.character(input$daterange) })
+  output$daterange2 <- renderText({ as.character(input$daterange2) })
 })
--- datepicker/ui.r
+++ datepicker/ui.r
@@ -1,15 +1,25 @@
 shinyUI(pageWithSidebar(
   headerPanel("Date Picker"),
   sidebarPanel(
-    dateInput('date', label = 'Date input'),
-    dateInput('date2', label = 'Date input 2'),
-    dateRangeInput('daterange', label = 'Date range input'),
+    dateInput('date', label = 'Date input: yyyy-mm-dd'),
 
-    dateRangeInput('daterange2', label = 'Date range input 2',
-      sep = " to ",
-      start = "2012-02-29", end = "2012-06-13",
-      min   = "2000-01-01", max = "2016-12-31")
+    dateInput('date2',
+      label = 'Date input 2: mm/dd/yy, zh-TW, range limit, weekstart=3',
+      value = "2012-02-29",
+      min = "2012-02-28", max = "2013-03-02", format = "%m/%d/%y",
+      startview = 'year', language = 'zh-TW', weekstart = 3),
+
+    dateRangeInput('daterange', label = 'Date range input: yyyy-mm-dd',
+      start = "2012-02-29", end ="2012-03-12"),
+
+    dateRangeInput('daterange2',
+      label = 'Date range input 2: mm/dd/yy, fr, range limit, weekstart=3',
+      start = as.Date('2012-02-29'), end = as.Date('2012-03-12'),
+      min = "2012-02-28", max = "2013-03-02",
+      separator = " - ",
+      format = "%m/%d/%y", startview = 'year', language = 'fr', weekstart = 3)
   ),
+
   mainPanel(
     verbatimTextOutput("date"),
     verbatimTextOutput("date2"),
