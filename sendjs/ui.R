824c824e82d5df0080667d5e35c94ed42c9da18c
Winston Chang <winston@stdout.org>
2013-06-28 14:01:49 -0500

    Add source URL

--- sendjs/ui.R
+++ sendjs/ui.R
@@ -1,7 +1,10 @@
 shinyUI(pageWithSidebar(
   headerPanel("Send Javascript test"),
   sidebarPanel(
-    sliderInput("controller", "Controller:", min = 1, max = 20, value = 15)
+    sliderInput("controller", "Controller:", min = 1, max = 20, value = 15),
+    br(),
+    a(href = "https://github.com/wch/testapp/tree/master/sendjs",
+      "Source code")
   ),
   mainPanel(
     h2("Filler text"),
