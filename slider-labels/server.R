6fa042e6a72f9fe0cefac4a6d3a516e9f88929cd
Winston Chang <winston@stdout.org>
2013-08-09 15:16:09 -0500

    Add link to source code

--- slider-labels/server.R
+++ slider-labels/server.R
@@ -3,7 +3,7 @@ shinyServer(function(input, output) {
   output$values <- renderTable({
     data.frame(
       Name = c("log"),
-      Value = as.character(c(input$log)), 
+      Value = as.character(c(input$log)),
       stringsAsFactors=FALSE
     )
   })
--- slider-labels/ui.R
+++ slider-labels/ui.R
@@ -5,10 +5,13 @@ shinyUI(pageWithSidebar(
     # Manual labels (to appear logarithmic)
     sliderInput("log", "Logarithmic (tick labels only):",
                 min = 1, max = 4, value = 1, step = 1,
-                ticks = c(.9, .99, .999, .9999))    
+                ticks = c(.9, .99, .999, .9999)),
+
+    br(),
+    a(href = "https://github.com/wch/testapp/tree/master/slider-labels",
+      "Source code")
   ),
-  
-  # Show a table summarizing the values entered
+
   mainPanel(
     tableOutput("values")
   )
